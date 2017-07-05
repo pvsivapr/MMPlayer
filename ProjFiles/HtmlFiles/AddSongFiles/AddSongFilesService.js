7161a38287f6c16aafcad54c992a9a139c7f99ca
P.V.S.P.Reddy <vekatasivaprasadreddypulagam@gmail.com>
2017-07-05 20:15:54 +0530

    Committing Changes

--- ProjFiles/HtmlFiles/AddSongFiles/AddSongFilesService.js
+++ ProjFiles/HtmlFiles/AddSongFiles/AddSongFilesService.js
@@ -1,15 +1,45 @@
-SubmitSong()
-{$id=$_POST["itemId"];
-$mId=$_POST["movieId"];
-$mName=$_POST["movieName"];
-$mNoSongs=$_POST["noOfSongs"];
-$mLogo=$_POST["movieLogoURL"];
+function SubmitMovie()
+{
+  alert("clicked");
+  // var obj, dbParam, xmlhttp, myObj, x, txt = "";
+  var x1 = document.forms["addMovieForm"]["itemId"].value;
+  var x2 = document.forms["addMovieForm"]["movieId"].value;
+  var x3 = document.forms["addMovieForm"]["movieName"].value;
+  var x4 = document.forms["addMovieForm"]["noOfSongs"].value;
+  var x5 = document.forms["addMovieForm"]["movieLogoURL"].value;
+  var postObject = { "id":x1, "movie_id":x2, "movie_name":x3, "no_of_songs":x4, "movie_logo_url":x5 };
+  dbParam = JSON.stringify(postObject);
+  dbParam2 = 'id='+x1+'&movie_id='+x2+'&movie_name='+x3+'&no_of_songs='+x4+'&movie_logo_url='+x5;
+  xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 
-  var x1 = document.forms["logForm"]["itemId"].value;
-  var x2 = document.forms["logForm"]["movieId"].value;
-  var x3 = document.forms["logForm"]["movieName"].value;
-  var x4 = document.forms["logForm"]["noOfSongs"].value;
-  var x5 = document.forms["logForm"]["movieLogoURL"].value;
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+//          var myObj = JSON.parse(this.response);
+          var data1 = this.response;
+          var data4 = this.responseText;
+          alert(data1);
+      }
+      else if(this.readyState == 4){
+          var data1 = this.response;
+          var data4 = this.responseText;
+          var data5 = xmlhttp.status;
+      }
+  };
+  xmlhttp.open("POST", "AddSongJSPHPService.php");
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//  xmlhttp.setRequestHeader("Content-type", "application/json");
+xmlhttp.setRequestHeader("Cache-Control", "no-cache");
+xmlhttp.withCredentials = true;
+xmlhttp.send("x=" + dbParam);
+}
+
+function SubmitSong()
+{
+  var x1 = document.forms["addSongForm"]["itemId"].value;
+  var x2 = document.forms["addSongForm"]["movieId"].value;
+  var x3 = document.forms["addSongForm"]["movieName"].value;
+  var x4 = document.forms["addSongForm"]["noOfSongs"].value;
+  var x5 = document.forms["addSongForm"]["movieLogoURL"].value;
   var postObject = { "id":x1, "movie_id":x2, "movie_name":x3, "no_of_songs":x4, "movie_logo_url":x5 };
   dbParam = JSON.stringify(postObject);
   xmlhttp = new XMLHttpRequest();
@@ -21,7 +51,8 @@ $mLogo=$_POST["movieLogoURL"];
           document.alert(data1);
       }
   };
-  xmlhttp.open("POST", "AddSongFilesJSPHPService.php", true);
+  xmlhttp.open("POST", "AddSongJSPHPService.php", true);
+//  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xmlhttp.setRequestHeader("Content-type", "application/json");
   xmlhttp.send("x=" + dbParam);
 }
--- ProjFiles/HtmlFiles/AddSongFiles/AddSongJSPHPService.php
+++ ProjFiles/HtmlFiles/AddSongFiles/AddSongJSPHPService.php
@@ -6,30 +6,28 @@ $password = "\$iva02071992mmplayer";
 $dbName = "mmplayerdb";
 $host = 3306;
 
-echo json_encode($outp);
 try
-    {
-      // header("Content-Type: application/json; charset=UTF-8");
-      // $obj = json_decode($_POST["x"], false);
-      // $conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
-      // $result = $conn->query("SELECT name FROM ".$obj->table." LIMIT ".$obj->limit);
-      // $outp = array();
-      // $outp = $result->fetch_all(MYSQLI_ASSOC);
-      $connn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
-      $connn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sql = "INSERT INTO mmplayerdb.mmplayermovies (id, movieId, movieName, movieLogoURL, noOfSongs) VALUES ('$obj->id', '$obj->movie_id', '$obj->movie_name', '$obj->no_of_songs', '$obj->movie_logo_url')";
-      $connn->exec($sql);
-      echo "<script type='text/javascript'>alert('Added successfully :)')</script>";
-       // echo "Added successfully";
-    }
-    catch(PDOException $e)
-    {
-      $errMsgss = "Connection failed: " . $e->getMessage();
-      // echo "<script type='text/javascript'>alert('".$errMsgss."')</script>";
-      echo "Connection failed: " . $e->getMessage();
-      /*echo ('<script language="javascript">');
-      echo ('alert(<?php $e->getMessage();  ?>)');  //not showing an alert box.
-      echo ('</script>');
-      */
-    }
+{
+  header("Content-Type: application/json; charset=UTF-8");
+      $obj = json_decode($_POST["x"], false);
+    // echo "successful1";
+    $sql = "INSERT INTO mmplayerdb.mmplayermovies (id, movieId, movieName, noOfSongs, movieLogoURL) VALUES ('$obj->id', '$obj->movie_id', '$obj->movie_name', '$obj->no_of_songs', '$obj->movie_logo_url')";
+    // echo "successful2";
+       $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+    // echo "successful3";
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    // echo "successful4";
+   $conn->exec($sql);
+    // echo "successful5";
+    $result = array("message"=>"User Registration is successful","code"=>"1","Result"=>"worked successful");
+    echo json_encode($result);
+}
+catch(PDOException $e)
+{
+  // $result = array("message"=>"User Registration is not successful","code"=>"1","Result"=>$e->getMessage);
+  // echo json_encode($result);
+  echo 'Exception -> ';
+    var_dump($e->getMessage());
+}
 ?>
